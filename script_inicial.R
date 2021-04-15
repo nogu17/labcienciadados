e359c7a0e6c9b160160bdcbc322e62671a0f7d65
Deise Nogueira <deise17nogueira@hotmail.com>
2021-04-15 17:10:20 -0300

    atualização do banco de dados

--- script_inicial.R
+++ script_inicial.R
@@ -1,7 +1,9 @@
 usethis::create_project("C:\\projetor\\labcienciadados")
 
 #### Criar a estrutura git para esse projeto####
-usethis::use_git()
-usethis::create_github_token()
-usethis::use_github()
-gitcreds::gitcreds_set(url="http://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")
+
+##usethis::use_git()
+##usethis::use_github()
+##usethis::create_github_token()
+##gitcreds::gitcreds_set() #nao precisa de URL'
+
