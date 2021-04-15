1dfe775477005a8d05efcc1e7bdc8160b64bb6c5
Deise Nogueira <deise17nogueira@hotmail.com>
2021-04-15 14:24:58 -0300

    Primeiro commit

---
+++ script_inicial.R
@@ -0,0 +1,7 @@
+usethis::create_project("C:\\projetor\\labcienciadados")
+
+#### Criar a estrutura git para esse projeto####
+usethis::use_git()
+usethis::create_github_token()
+usethis::use_github()
+gitcreds::gitcreds_set(url="http://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")
